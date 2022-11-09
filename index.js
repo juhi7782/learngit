5d8220821d7a0663e0976e7c505ada4e52d8042b
juhi7782 <108575713+juhi7782@users.noreply.github.com>
2022-11-09 17:37:04 +0530

    changes

---
+++ index.js
@@ -0,0 +1,38 @@
+console.log("hello")
+
+const hourE1=document.getElementById("hour");
+
+const minuteE1=document.getElementById("minutes");
+
+const secondE1=document.getElementById("seconds");
+
+const hourEl = document.getElementById("hour");
+const minuteEl = document.getElementById("minutes");
+const secondEl = document.getElementById("seconds");
+const ampmEl = document.getElementById("ampm");
+
+function updateClock() {
+  let h = new Date().getHours();
+  let m = new Date().getMinutes();
+  let s = new Date().getSeconds();
+  let ampm = "AM";
+
+  if (h > 12) {
+    h = h - 12;
+    ampm = "PM";
+  }
+
+  h = h < 10 ? "0" + h : h;
+  m = m < 10 ? "0" + m : m;
+  s = s < 10 ? "0" + s : s;
+
+  hourEl.innerText = h;
+  minuteEl.innerText = m;
+  secondEl.innerText = s;
+  ampmEl.innerText = ampm;
+  setTimeout(() => {
+    updateClock();
+  }, 1000);
+}
+
+updateClock();
